e4f33da3bc5cad29187f95dd6c2f9dcaf2b62389
Taha haghgoo <93734684+thaghgoo@users.noreply.github.com>
2024-11-29 03:11:39 +0330

    initializing newComment method
    This method sends a list containing the names of the professors . I will update how to get the names of the professors when i got the data

--- app/bot_handlers.py
+++ app/bot_handlers.py
@@ -1,5 +1,13 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
 async def startCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    
+async def newComment(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    professors_name = ["professor1" , "professor2" , "professor3"]
+    keyboard = [
+        [InlineKeyboardButton(name, callback_data=name)] for name in professors_name
+    ]
+    reply = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Choose a professor:", reply_markup=reply)
