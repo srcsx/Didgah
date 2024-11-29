a7139a72612aa46c0e0028307db98a1b347a658a
Taha haghgoo <93734684+thaghgoo@users.noreply.github.com>
2024-11-29 22:07:04 +0330

    lessonFlow added
    this method will send a flow that have one column for list of lessons name

--- app/bot_handlers.py
+++ app/bot_handlers.py
@@ -4,7 +4,7 @@ from telegram.ext import ContextTypes
 async def startCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
     
-async def newComment(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def profNameFlow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     professors_name = ["professor1" , "professor2" , "professor3"]
     keyboard = [
         [InlineKeyboardButton(name, callback_data=name)] for name in professors_name
@@ -12,7 +12,15 @@ async def newComment(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
     reply = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Choose a professor:", reply_markup=reply)
     
-async def newCommentClickHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def lessonNameFlow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    lessons = ["1" , "2" , "3"]
+    keyboard = [
+        [InlineKeyboardButton(lesson, callback_data=lesson)] for lesson in lessons
+    ]
+    reply = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Choose a lesson:", reply_markup=reply)
+    
+async def FlowButtonHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
     selected_name = query.data
