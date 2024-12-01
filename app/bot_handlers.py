c00cc25d32c97efe23240d409f1c6d1ad86a7569
Taha haghgoo <93734684+thaghgoo@users.noreply.github.com>
2024-12-01 21:23:10 +0330

    change profFlow and lessonFLow
    flows have been changed so we can use inline query on flows button clicking

--- app/bot_handlers.py
+++ app/bot_handlers.py
@@ -4,21 +4,22 @@ from telegram.ext import ContextTypes
 async def startCommand(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
     
-async def profNameFlow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    professors_name = ["professor1" , "professor2" , "professor3"]
+professors_name = ["professor1" , "professor2" , "professor3"]
+lessons = ["1", "2", "3"]
+
+async def profFlow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
-        [InlineKeyboardButton(name, callback_data=f"professor:{name}")] for name in professors_name
+        [InlineKeyboardButton("Choose a professor" , switch_inline_query_current_chat="#professors")]
     ]
     reply = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Choose a professor:", reply_markup=reply)
+    await update.message.reply_text("Click the button below to choose a professor:", reply_markup=reply)
     
-async def lessonNameFlow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    lessons = ["1", "2", "3"]
+async def lessonFlow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
-        [InlineKeyboardButton(lesson, callback_data=f"lesson:{lesson}")] for lesson in lessons
+        [InlineKeyboardButton("Choose a lesson" , switch_inline_query_current_chat="#lessons")]
     ]
     reply = InlineKeyboardMarkup(keyboard)
-    await update.callback_query.edit_message_text("Choose a lesson:", reply_markup=reply)
+    await update.message.reply_text("Click the button below to choose a lesson:", reply_markup=reply)
     
 async def FlowButtonHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
