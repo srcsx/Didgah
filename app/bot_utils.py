8e5f33e5c045a49398e00241c6d352ab61ae4453
Taha haghgoo <93734684+thaghgoo@users.noreply.github.com>
2024-12-05 03:06:23 +0330

    methods and imports added

--- app/bot_utils.py
+++ app/bot_utils.py
@@ -1,7 +1,7 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, filters, CommandHandler, InlineQueryHandler, MessageHandler, CallbackQueryHandler
 from config.init import TELEGRAM_BOT_TOKEN
 from config.constants import START_COMMAND
-from .bot_handlers import startCommand
+from .bot_handlers import startCommand, inlineQuery, messageHandler, CommentHandler, unknownHandler
 
 def run():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
@@ -12,3 +12,7 @@ def run():
 
 def setHandlers(app):
     app.add_handler(CommandHandler(START_COMMAND, startCommand))
+    app.add_handler(InlineQueryHandler(inlineQuery))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, messageHandler))
+    app.add_handler(CallbackQueryHandler(CommentHandler, pattern="^comment$"))
+    app.add_handler(MessageHandler(filters.COMMAND, unknownHandler))
